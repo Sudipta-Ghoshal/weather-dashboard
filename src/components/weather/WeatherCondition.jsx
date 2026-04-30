fc7122fe560194c6ecf2d361e650e2479e7b728d
Sudipta Ghoshal <ghoshalsudipta36@gmail.com>
2026-04-30 13:19:24 +0530

    dynamic weather data bind

--- src/components/weather/WeatherCondition.jsx
+++ src/components/weather/WeatherCondition.jsx
@@ -1,48 +1,52 @@
+import { useContext } from "react";
 import cloudIcon from "../../assets/icons/cloud.svg";
 import humidityIcon from "../../assets/icons/humidity.svg";
 import tempMaxIcon from "../../assets/icons/temp-max.svg";
 import tempMinIcon from "../../assets/icons/temp-min.svg";
 import windIcon from "../../assets/icons/wind.svg";
+import WeatherContext from "../../context";
 
 export default function WeatherCondition() {
+  const { weather } = useContext(WeatherContext);
+
   return (
     <div>
       <p className="text-sm lg:text-lg font-bold uppercase mb-8">
-        thunderstorm with light drizzle
+        {weather.climate}
       </p>
       <ul className="space-y-6 lg:space-y-6">
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Temp max</span>
           <div className="inline-flex space-x-4">
-            <p>19°</p>
+            <p>{Math.round(weather.maxTemperature)}°</p>
             <img src={tempMaxIcon} alt="temp-max" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Temp min</span>
           <div className="inline-flex space-x-4">
-            <p>19°</p>
+            <p>{Math.round(weather.minTemperature)}°</p>
             <img src={tempMinIcon} alt="temp-min" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Humadity</span>
           <div className="inline-flex space-x-4">
-            <p>58%</p>
+            <p>{weather.humidity}%</p>
             <img src={humidityIcon} alt="humidity" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Cloudy</span>
           <div className="inline-flex space-x-4">
-            <p>86%</p>
+            <p>{weather.cloudiness}%</p>
             <img src={cloudIcon} alt="cloudy" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Wind</span>
           <div className="inline-flex space-x-4">
-            <p>5km/h</p>
+            <p>{weather.windSpeed}km/h</p>
             <img src={windIcon} alt="wind" />
           </div>
         </li>
--- src/components/weather/WeatherHeading.jsx
+++ src/components/weather/WeatherHeading.jsx
@@ -1,18 +1,21 @@
+import { useContext } from "react";
 import cloudIcon from "../../assets/cloud.svg";
 import pinIcon from "../../assets/pin.svg";
+import WeatherContext from "../../context";
 
 export default function WeatherHeading() {
+  const { weather } = useContext(WeatherContext);
   return (
     <div>
-      <div className="max-md:flex items-center justify-between md:-mt-10">
+      <div className="max-md:flex items-center justify-between">
         <img src={cloudIcon} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(weather.temperature)}°
           </h1>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={pinIcon} alt="location" />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{weather.location}</h2>
           </div>
         </div>
       </div>
