9dddd0ccecfd3b54e5eec0a3a320c3ffe19829fe
Sudipta Ghoshal <ghoshalsudipta36@gmail.com>
2026-05-04 10:24:19 +0530

    Refactors Search for debounced global input

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,33 +1,36 @@
 import searchIcon from "../../assets/search.svg";
 import { useContext } from "react";
 import { LocationContext } from "../../context";
-import { useState } from "react";
 import { getLocationByName } from "../../data/location-data";
+import useDebounce from "../../hooks/useDebounce";
 
 export default function Search() {
-  const [serchTerm, setSearchTerm] = useState("");
-  const { setSelectedLocation } = useContext(LocationContext);
+  const { searchTerm, setSearchTerm, setSelectedLocation } =
+    useContext(LocationContext);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const getLocation = getLocationByName(serchTerm);
+  const debounceSerch = useDebounce((value) => {
+    const getLocation = getLocationByName(value);
     setSelectedLocation(getLocation);
+  }, 500);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setSearchTerm(value);
+    debounceSerch(value);
   }
 
   return (
-    <form action="#" onSubmit={handleSubmit}>
+    <form action="#">
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
           className="bg-transparent placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
           required
-          value={serchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          onChange={handleChange}
         />
-        <button type="submit">
-          <img src={searchIcon} alt="search" />
-        </button>
+        <img src={searchIcon} alt="search" />
       </div>
     </form>
   );
